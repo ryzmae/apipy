9d73a29b1a2385ca781f32becf9074a550eb12cf
Juwen Nagpal <Juwennagpal6@gmail.com>
2023-02-03 13:23:28 +0100

    Updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="MerlBot API",
+    description="MerlBot API",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    version="0.0.1"
+)
 
 @app.get("/")
 def root():
